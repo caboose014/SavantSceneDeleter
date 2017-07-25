c4e9a3ffd2a0e052ddc1e0ef4a56ff8ce2797aa3
Jordan McDonald <j_14@me.com>
2017-07-25 15:10:47 +1200

    Update deletescene.py

--- deletescene.py
+++ deletescene.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#     
+#
 #     'SavantSceneDelete'
 #     Copyright (C) '2017' J14 Systems
 #
@@ -20,72 +20,85 @@ import sqlite3
 import os.path
 from subprocess import call
 
-if os.path.isfile('/Users/RPM/Library/Application Support/RacePointMedia/statusfiles/dis/dashboard.sqlite'):
-    hosttype = "pro"
-    database = "/Users/RPM/Library/Application Support/RacePointMedia/statusfiles/dis/dashboard.sqlite"
-elif os.path.isfile('/data/RPM/GNUstep/Library/ApplicationSupport/RacePointMedia/statusfiles/dis/dashboard.sqlite'):
-    hosttype = "smart"
-    database = "/data/RPM/GNUstep/Library/ApplicationSupport/RacePointMedia/statusfiles/dis/dashboard.sqlite"
-else:
-    hosttype = "none"
+try:
+    if os.path.isfile('/Users/RPM/Library/Application Support/RacePointMedia/statusfiles/dis/dashboard.sqlite'):
+        hosttype = "pro"
+        database = "/Users/RPM/Library/Application Support/RacePointMedia/statusfiles/dis/dashboard.sqlite"
+    elif os.path.isfile('/data/RPM/GNUstep/Library/ApplicationSupport/RacePointMedia/statusfiles/dis/dashboard.sqlite'):
+        hosttype = "smart"
+        database = "/data/RPM/GNUstep/Library/ApplicationSupport/RacePointMedia/statusfiles/dis/dashboard.sqlite"
+    else:
+        hosttype = "none"
+        while True:
+            databasepath = raw_input("Type the path to your dashboard.sqlite database: ")
+            if os.path.isfile(databasepath):
+                database = databasepath
+                break
+
+    print "Running on a host type of: %s" % hosttype
+
+    if hosttype != "none":
+        while True:
+            confirm1 = raw_input("Savant needs to be disabled to continue, type 'yes' to confirm: ")
+            if confirm1.lower() == 'yes':
+                call('/usr/local/bin/rpm/stopSavantSystem')
+                break
+
     while True:
-        databasepath = raw_input("Type the path to your dashboard.sqlite database: ")
-        if os.path.isfile(databasepath):
-            database = databasepath
+        dashbaord = sqlite3.connect(database)
+        query1 = dashbaord.execute("SELECT * FROM Scenes")
+        scenenum = 1
+        selection = 0
+        scenes = {}
+
+        print "Found Scenes:"
+        print "----------------------------------------"
+        for row in query1:
+            print "%s: %s [Active In: %s]" % (scenenum, row[1], row[3])
+            scenes[scenenum] = row
+            scenenum += 1
+        print "----------------------------------------"
+        while True:
+            try:
+                selection = int(raw_input("Type a scene number to delete it: "))
+                if selection in scenes:
+                    break
+                else:
+                    raise KeyError
+            except ValueError:
+                print "Please enter a number from 1 to %s" % (scenenum - 1)
+            except KeyError:
+                print "Please enter a number from 1 to %s" % (scenenum - 1)
+
+        print "----------------------------------------"
+        print "You have chosent to delete \"%s\" which is active in the following zone(s): %s" \
+              % (scenes[selection][1], scenes[selection][3])
+        print "----------------------------------------"
+        confirm = raw_input("Type 'yes' to confirm: ")
+        print "----------------------------------------"
+        if confirm.lower() == 'yes':
+            dashbaord.execute("DELETE from Scenes WHERE identifier = '%s'" % scenes[selection][0])
+            dashbaord.execute("DELETE from SceneUserMap WHERE sceneIdentifier = '%s'" % scenes[selection][0])
+            dashbaord.execute("DELETE from SceneDefinitions WHERE sceneIdentifier = '%s'" % scenes[selection][0])
+            dashbaord.commit()
+            print "Scene \"%s\" deleted" % scenes[selection][1]
+
+
+        print "----------------------------------------"
+        confirm2 = raw_input("Would you like to delete any other scenes? yes or no: ")
+        print "----------------------------------------"
+        if confirm2.lower() != 'yes':
             break
 
-print "Running on a host type of: %s" % hosttype
 
-if hosttype != "none":
-    while True:
-        confirm1 = raw_input("Savant needs to be disabled to continue, type 'yes' to confirm: ")
-        if confirm1.lower() == 'yes':
-            call('/usr/local/bin/rpm/stopSavantSystem')
-            break
+    if hosttype != "none":
+        while True:
+            confirm1 = raw_input("Savant can now be restarted, type 'yes' to confirm: ")
+            if confirm1.lower() == 'yes':
+                call('/usr/local/bin/rpm/startSavantSystem')
+                break
+            elif confirm1.lower() == "no":
+                print "Not restarting Savant system. This will have to be done manually"
 
-dashbaord = sqlite3.connect(database)
-query1 = dashbaord.execute("SELECT * FROM Scenes")
-scenenum = 1
-selection = 0
-scenes = {}
-
-print "Found Scenes:"
-print "----------------------------------------"
-for row in query1:
-    print "%s: %s [Active In: %s]" % (scenenum, row[1], row[3])
-    scenes[scenenum] = row
-    scenenum += 1
-print "----------------------------------------"
-while True:
-    try:
-        selection = int(raw_input("Type a scene number to delete it: "))
-        if selection in scenes:
-            break
-        else:
-            raise KeyError
-    except ValueError:
-        print "Please enter a number from 1 to %s" % (scenenum - 1)
-    except KeyError:
-        print "Please enter a number from 1 to %s" % (scenenum - 1)
-
-print "----------------------------------------"
-print "You have chosent to delete \"%s\" which is active in the following zone(s): %s" \
-      % (scenes[selection][1], scenes[selection][3])
-print "----------------------------------------"
-confirm = raw_input("Type 'yes' to confirm: ")
-print "----------------------------------------"
-if confirm.lower() == 'yes':
-    dashbaord.execute("DELETE from Scenes WHERE identifier = '%s'" % scenes[selection][0])
-    dashbaord.execute("DELETE from SceneUserMap WHERE sceneIdentifier = '%s'" % scenes[selection][0])
-    dashbaord.execute("DELETE from SceneDefinitions WHERE sceneIdentifier = '%s'" % scenes[selection][0])
-    dashbaord.commit()
-    print "Scene \"%s\" deleted" % scenes[selection][1]
-
-if hosttype != "none":
-    while True:
-        confirm1 = raw_input("Savant can now be restarted, type 'yes' to confirm: ")
-        if confirm1.lower() == 'yes':
-            call('/usr/local/bin/rpm/startSavantSystem')
-            break
-        elif confirm1.lower() == "no":
-            print "Not restarting Savant system. This will have to be done manually"
+except KeyboardInterrupt:
+    print "Thanks for using J14 Systems Scene Deletion tool!"
